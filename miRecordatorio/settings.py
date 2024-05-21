a72df52cb1e573ada3a9d8e21a9086460d60fbc8
lina viveros <lina.viverosm@campusucc.edu.co>
2024-05-21 12:20:39 -0500

    subiendo base

--- miRecordatorio/settings.py
+++ miRecordatorio/settings.py
@@ -123,8 +123,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "mydatabase",
-        "USER": "mydatabaseuser",
-        "PASSWORD": "mypassword",
+        "USER": "ckiontfbrv",
+        "PASSWORD": "m4552cZk$lmqBjq2h",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
